185fa2dec8beca1c5040aa5aae0fb506e1576bde
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-05 14:20:38 -0400

    Generate random number by computer

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Create random number (1,2 or 3) generator for computer
+function getComputerChoice(){
+    let choice = Math.floor(Math.ramdom() * 3);
+    return choice;
+}
 
-// Let player choose 1, 2 or 3.
+// Let player choose rock, paper or scissors.
+
+// Convert selection to number.
+
 
 // Compare and choose who won.
+
+// Generate random number for computer
+// If number 
