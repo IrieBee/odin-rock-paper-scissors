08bd41e032d8b477d4f3472b71ca446128651443
itsy bitsy <119555750+ltsyBitsy@users.noreply.github.com>
2023-07-18 23:27:28 -0400

    Restart button fixed. Change alert to textContent.

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 const announce = document.querySelector('h2');
 const score = document.querySelector('h5');
+const result = document.querySelector('h3');
+const restart = document.querySelector('#restart');
+
 // Player choice
-const playerChoice = Array.from(document.querySelectorAll('button'));
+const playerChoice = Array.from(document.querySelectorAll('.button'));
 let computerScore = 0;
 let playerScore = 0;
 
+
 playerChoice.forEach(choice => {
     choice.onclick =  () => {
-        let playerSelection =  playerChoice.indexOf(choice) + 1;
-        console.log('playerSelection: ', playerSelection);
-
-        const computerSelection = getComputerChoice();
-        console.log("computerSelection: " , computerSelection);
+        if (playerScore < 5 && computerScore < 5) {
+            let playerSelection =  playerChoice.indexOf(choice) + 1;
+    
+            const computerSelection = getComputerChoice();
         
-        playRound(computerSelection, playerSelection);
-            console.log(computerScore, playerScore);
+            playRound(computerSelection, playerSelection);
 
-        if (playerScore == 5 || computerScore == 5) {
-            if (playerScore > computerScore) {
-                    alert("Human : " + playerScore + ".  Robot : " + computerScore + ".  Human wins!");
-                location.reload();
-            }
-            else if (playerScore < computerScore) {
-                alert("Human : " + playerScore + ".  Robot : " + computerScore + " .  Robot wins!");
-                location.reload();
-            }
-            else alert("Human : " + playerScore + ".  Robot : " + computerScore +  ".  It's a tie!");
-            location.reload();
-            }
-            console.log('computer : player  ', computerScore, playerScore);
+            if (playerScore === 5 || computerScore === 5) {
+                if (playerScore > computerScore) {
+                    announce.textContent = "Human:" + playerScore + ". Robot:" + computerScore + ". Human wins!";
+                }
+                else if (playerScore < computerScore) {
+                    announce.textContent = "Human:" + playerScore + ". Robot:" + computerScore + ". Robot wins!";
+                }
+                else 
+                    announce.textContent = "Human:" + playerScore + ". Robot:" + computerScore + ". It's a tie!";
+                }
+        }            
     }
 })
+
+// Button restart
+restart.addEventListener('click', reset);
 
 
 // Create random number (1,2 or 3) generator for computer
@@ -42,45 +45,49 @@ function getComputerChoice(){
 // Compare results
 function playRound(computer, player) {
     if (computer === player) {
-        announce.textContent = 'It\'s a tie!';
+        result.textContent = 'It\'s a tie!';
         playerScore ++;
         computerScore ++;
         score.textContent = playerScore + " : " + computerScore;
     }
     else if (computer === 1 && player === 2) {
-        announce.textContent = "You win! Paper beats Rock.";
+        result.textContent = "You win! Paper beats Rock.";
         playerScore ++;
         score.textContent = playerScore + " : " + computerScore;
     }
     else if (computer === 2 && player === 3) {
-        announce.textContent = "You win! Scissors beat Paper.";
+        result.textContent = "You win! Scissors beat Paper.";
         playerScore ++;
         score.textContent = playerScore + " : " + computerScore;
     }
     else if (computer === 3 && player === 1) {
-        announce.textContent = "You win! Rock beats Scissors.";
+        result.textContent = "You win! Rock beats Scissors.";
         playerScore ++;
         score.textContent = playerScore + " : " + computerScore;
     }
-    
     else if (computer === 2 && player === 1) {
-        announce.textContent = "Robot wins! Paper beats Rock.";
+        result.textContent = "Robot wins! Paper beats Rock.";
         computerScore ++;
         score.textContent = playerScore + " : " + computerScore;
-        // return computerScore;
     }
     else if (computer === 3 && player === 2) {
-        announce.textContent = "Robot wins! Scissors beat Paper.";
+        result.textContent = "Robot wins! Scissors beat Paper.";
         computerScore ++;
         score.textContent = playerScore + " : " + computerScore;
-        // return computerScore;
     }
     else if (computer === 1 && player === 3) {
-        announce.textContent = "Robot wins! Rock beats Scissors.";
+        result.textContent = "Robot wins! Rock beats Scissors.";
         computerScore ++;
         score.textContent = playerScore + " : " + computerScore;
-        // return computerScore;
     }
 }
 
+// Restart game
+function reset() {
+    computerScore = 0;
+    playerScore = 0;
+    score.textContent = ' 0 : 0 ';
+    announce.textContent = 'Make your choice!';
+    result.textContent = 'Score';
+}
 
